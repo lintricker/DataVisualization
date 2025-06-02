d7917661cd147cc515f35d32f5a03fd3b9f0efe2
lintricker <alina.ishem01@gmail.com>
2025-06-02 21:58:57 +0400

    SidebarLayout

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <p><a href=''>check</a></p> 
+      <p><a href=''>check</a></p> 
+      <p><a href=''>check</a></p> 
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/MenuContent.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import { FolderCopyRounded, SnippetFolder } from '@mui/icons-material';
+
+const mainListItems = [
+  { text: 'Главная', icon: <HomeRoundedIcon /> },
+  { text: 'Черновики', icon: <SnippetFolder /> },
+  { text: 'Мои папки', icon: <FolderCopyRounded /> }
+];
+
+const secondaryListItems = [
+  { text: 'Настройки', icon: <SettingsRoundedIcon /> },
+  { text: 'О программе', icon: <InfoRoundedIcon /> }
+];
+
+export default function MenuContent() {
+  return (
+    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+      <List dense>
+        {mainListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton selected={index === 0}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <List dense>
+        {secondaryListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
+  );
+}
